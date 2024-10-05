b6c78c483a1cfe5c4667996eea7b332a94f1360d
CharelSuarez <114007611+CharelSuarez@users.noreply.github.com>
2024-10-05 14:17:17 -0400

    Create game gamemodes and difficulty levels.

---
+++ src/game/game.tsx
@@ -0,0 +1,81 @@
+import { TimerIcon, XIcon, CloudLightningIcon } from "lucide-react"
+import { ForwardRefExoticComponent } from "react"
+
+class Rule {
+    icon: ForwardRefExoticComponent<any>
+    description: string
+    constructor(icon: ForwardRefExoticComponent<any>, description: string) {
+        this.icon = icon
+        this.description = description
+    }
+}
+
+class BrainStormGamemode {
+    id: string
+    name: string
+    image: string
+    rules: Rule[]
+    constructor(id: string, name: string, image: string, rules: Rule[]) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.rules = rules
+    }
+
+    getRules() {
+        return this.rules
+    }
+
+    getImage() {
+        return this.image
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+}
+
+export const A_RAINY_DAY = new BrainStormGamemode("a-rainy-day", "A Rainy Day", "rainy_day.png", 
+    [new Rule(TimerIcon, "No time pressure."), new Rule(XIcon, "Incorrect answers intensify the storm.")])
+
+export const A_STORMY_NIGHTMARE = new BrainStormGamemode("a-stormy-nightmare", "A Stormy Nightmare", "stormy_nightmare.png",
+    [new Rule(TimerIcon, "You have 5-10s to answer each question."), new Rule(CloudLightningIcon, "Running out of time might be shocking.")])
+
+
+class BrainStormDifficulty {
+    id: string
+    name: string
+    timer: number
+    shock_level: number
+    constructor(id: string, name: string, timer: number, shock_level: number) {
+        this.id = id
+        this.name = name
+        this.timer = timer
+        this.shock_level = shock_level
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getTimer() {
+        return this.timer
+    }
+
+    getShockLevel() {
+        return this.shock_level
+    }
+}
+
+export const PEACEFUL = new BrainStormDifficulty("peaceful", "Peaceful", 10, 0)
+export const EASY = new BrainStormDifficulty("easy", "Easy", 10, 10)
+export const MEDIUM = new BrainStormDifficulty("medium", "Medium", 7, 20)
+export const HARD = new BrainStormDifficulty("hard", "Hard", 5, 30)
