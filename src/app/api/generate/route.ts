fdf0d204d8ca930d5eb1418d8a8fcd614fc1101e
CharelSuarez <114007611+CharelSuarez@users.noreply.github.com>
2024-10-09 19:32:38 -0400

    Discrete math lmao

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -21,13 +21,24 @@ export async function GET(request: NextRequest) {
 
     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+    let content = "Give me 10 " + difficulty + " question about topic " + topic + " with 4 options that are very close to the correct answer and the correct answer. " +
+    "Respond in an array of JSON objects in the exact form [{ \"question\": \"\", \"options\": [\"\", \"\", \"\", \"\"], \"answer\": \"\" }]. Please do not use any markup or any other formatting text."
+
+    if (topic === "Discrete Math") {
+      content = "Give me 10 " + difficulty + " question about first year level discrete math with 4 options that are very close to the correct answer and the correct answer. " +
+      "Respond in an array of JSON objects in the exact form [{ \"question\": \"\", \"options\": [\"\", \"\", \"\", \"\"], \"answer\": \"\" }]. Please do not " +
+      "use any markup or any other formatting text. For example include deductive reasoning with quantifiers like \"for all\" and \"there exists\". " +
+      "An example question would be: All birds eat at least one species of insect. At least one species of insect can fly. Therefore, all birds " +
+      "eat some flying beings. The correct answer would be: True. As for another example, you could also ask what the universe of discourse is for a given question.";
+    }
+
     const completion = await openai.chat.completions.create({
       model: "chatgpt-4o-latest",
       messages: [
         { role: "system", content: "You are an educational tutor." },
         {
           role: "user",
-          content: "Give me 10 " + difficulty + " question about topic " + topic + " with 4 options that are very close to the correct answer and the correct answer. Respond in an array of JSON objects in the exact form [{ \"question\": \"\", \"options\": [\"\", \"\", \"\", \"\"], \"answer\": \"\" }]. Please do not use any markup or any other formatting text.",
+          content: content,
         },
       ],
       temperature: 1.0,
